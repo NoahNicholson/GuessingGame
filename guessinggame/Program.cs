391cbb7bba5034b18eca37447b189265997bedeb
noahnicholson <noahnicholson239@yahoo.com>
2022-08-22 13:24:46 -0500

    phasethree

--- guessinggame/Program.cs
+++ guessinggame/Program.cs
@@ -1,24 +1,36 @@
 ﻿using System;
 
 int SecretNumber = 36;
+bool isAnswerCorrect = false;
+Guesses();
 
-CompareToNumber();
-
-void CompareToNumber()
+void Guesses()
 {
     Console.WriteLine("Guess The secret number?");
     string answer = Console.ReadLine(); //reads the user input
     int ParsedAnswer = int.Parse(answer); //parsing user answer to integer
+    GuessEqualToSecretNumber(ParsedAnswer);
+    int NumofGuesses = 0;
 
-    while (String.IsNullOrWhiteSpace(answer))
+    while (isAnswerCorrect == false && NumofGuesses < 3)
     {
         Console.WriteLine("Guess The secret number?");
         answer = Console.ReadLine(); //reads the user input
         ParsedAnswer = int.Parse(answer);
-    }
+        GuessEqualToSecretNumber(ParsedAnswer);
+        NumofGuesses += 1;
 
-    if (ParsedAnswer == SecretNumber)
+    }
+}
+void GuessEqualToSecretNumber(int answer)
+{
+    if (answer == SecretNumber)
     {
         Console.WriteLine($"Your guess is correct");
+        isAnswerCorrect = true;
+    }
+    else
+    {
+        Console.WriteLine("Try again");
     }
 }
