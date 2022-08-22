52853b4922cd298099a8633e6c48f23ea381ca4a
noahnicholson <noahnicholson239@yahoo.com>
2022-08-22 10:33:58 -0500

    phase

---
+++ guessinggame/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+
+Console.WriteLine("Guess The secret number?");
+string answer = Console.ReadLine(); //reads the user input
+int ParsedAnswer = int.Parse(answer); //parsing user answer to integer
+Console.WriteLine($"Your guess is {ParsedAnswer}");
