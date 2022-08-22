6a9a1e9f9e738a6c64789e1ee822cf72fefcc0cf
noahnicholson <noahnicholson239@yahoo.com>
2022-08-22 11:23:09 -0500

    phasetwo

--- guessinggame/Program.cs
+++ guessinggame/Program.cs
@@ -1,7 +1,24 @@
 ﻿using System;
 
+int SecretNumber = 36;
 
-Console.WriteLine("Guess The secret number?");
-string answer = Console.ReadLine(); //reads the user input
-int ParsedAnswer = int.Parse(answer); //parsing user answer to integer
-Console.WriteLine($"Your guess is {ParsedAnswer}");
+CompareToNumber();
+
+void CompareToNumber()
+{
+    Console.WriteLine("Guess The secret number?");
+    string answer = Console.ReadLine(); //reads the user input
+    int ParsedAnswer = int.Parse(answer); //parsing user answer to integer
+
+    while (String.IsNullOrWhiteSpace(answer))
+    {
+        Console.WriteLine("Guess The secret number?");
+        answer = Console.ReadLine(); //reads the user input
+        ParsedAnswer = int.Parse(answer);
+    }
+
+    if (ParsedAnswer == SecretNumber)
+    {
+        Console.WriteLine($"Your guess is correct");
+    }
+}
